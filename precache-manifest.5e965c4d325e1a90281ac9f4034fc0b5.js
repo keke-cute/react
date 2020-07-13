0bee5fc646b937c6a1f82bc5116f4a3488cdbcc5
Liu Shike <liushike1997@gmail.com>
2020-07-13 21:52:43 +0800

    Updates

---
+++ precache-manifest.5e965c4d325e1a90281ac9f4034fc0b5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471c616c37679887d8bd58c463ab0188",
+    "url": "./about.html"
+  },
+  {
+    "revision": "c3a7c20691337c678278ef74e2c2706d",
+    "url": "./components.html"
+  },
+  {
+    "revision": "6cd5141b766428eeeb1cd2d09450ac31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "874abd9ad173e083805487aae9b06ee6",
+    "url": "./myapp.html"
+  },
+  {
+    "revision": "556d9a1e64926520da61",
+    "url": "./static/css/about.67f0cb38.chunk.css"
+  },
+  {
+    "revision": "56f1076701141dd6f205",
+    "url": "./static/css/components.67f0cb38.chunk.css"
+  },
+  {
+    "revision": "ef25ec4043866aa367df",
+    "url": "./static/css/main.67f0cb38.chunk.css"
+  },
+  {
+    "revision": "4986d3b4c1b725e14931",
+    "url": "./static/css/myapp.67f0cb38.chunk.css"
+  },
+  {
+    "revision": "0988c5de006f11525764",
+    "url": "./static/js/0.5536d763.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/0.5536d763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556d9a1e64926520da61",
+    "url": "./static/js/about.dc35a2d8.chunk.js"
+  },
+  {
+    "revision": "56f1076701141dd6f205",
+    "url": "./static/js/components.4e9afb0e.chunk.js"
+  },
+  {
+    "revision": "ef25ec4043866aa367df",
+    "url": "./static/js/main.4173b439.chunk.js"
+  },
+  {
+    "revision": "4986d3b4c1b725e14931",
+    "url": "./static/js/myapp.5bf75892.chunk.js"
+  },
+  {
+    "revision": "3de92af412e95d05bc55",
+    "url": "./static/js/runtime-about.3239e584.js"
+  },
+  {
+    "revision": "3260f6978a5f46d0ff53",
+    "url": "./static/js/runtime-components.2dae396d.js"
+  },
+  {
+    "revision": "6d63815e7e1c3ce168d5",
+    "url": "./static/js/runtime-main.303cbdce.js"
+  },
+  {
+    "revision": "5663cedf117b9dadc9ec",
+    "url": "./static/js/runtime-myapp.abf0c832.js"
+  }
+]);
