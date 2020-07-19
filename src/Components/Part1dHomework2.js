0b3a928b4a254874c1134007e1286c1031a19f2e
Liu Shike <liushike1997@gmail.com>
2020-07-19 17:54:02 +0800

    add

--- src/Components/Part1dHomework2.js
+++ src/Components/Part1dHomework2.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react'
 
-
 const Part1dHomework2 = (props) => {
     const [selected, setSelected] = useState(0)
+    const [vote, setVote] = useState(new Array(5+1).join('0').split('').map(parseFloat))
+    let max = Math.max(...vote);
+    let maxIndex = vote.indexOf(max);
     return (
 	<div>
+	    <h2>Anecdote of the day</h2>
 	    {props.anecdotes[selected]}
-	    <br/>
+	    <br />
+	    <p>has {vote[selected]} votes</p>
+	    <br />
+	    <button onClick= { () => {
+			const copy = [...vote];
+			copy[selected] += 1;
+			setVote(copy);}}>vote</button>
 	    <button onClick= { () => setSelected(Math.floor(Math.random()*5)) }>next anecdotes</button>
+	    <h2>Anecdote with most votes</h2>
+	    {props.anecdotes[maxIndex]}
+	    <p>has {vote[maxIndex]} votes</p>	    
 	</div>
     )
 }
-
 
 
 export default Part1dHomework2
