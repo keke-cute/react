57c0a2500a97d4b9ab924345192b0f6c05e634fd
Liu Shike <liushike1997@gmail.com>
2020-07-31 17:10:33 +0800

    all

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import axios from 'axios'
 
 import './index.css';
 import Menu from './Components/menu'
 import Footer from './Components/footer'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-promise.then(response => {
-    console.log(response)
-})
-function App () {
+function App ({notes}) {
     return (
 	<header>
 	    <Menu />
 	    <div className="mydiv">this is my home page, now is empty,please use menu visit to other pages</div>
+	    {notes}
 	    <Footer />
 	</header>
     )
